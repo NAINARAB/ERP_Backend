7a4c6fe4c406507a45353083fe639be690e26e26
NAINARAB <karthickab77@gmail.com>
2023-11-16 10:20:21 +0530

    sys

--- index.mjs
+++ index.mjs
@@ -778,34 +778,6 @@ app.get('/api/pagerights', authenticateToken, async (req, res) => {
   }
 });
 
-app.get('/api/pagerights', authenticateToken, async (req, res) => {
-  const { menuid, menutype, usertype } = req.query;
-  const auth = req.header('Authorization');
-  try {
-    const pageright = new sql.Request(SMTERP);
-    pageright.input('User_Type', sql.NVarChar, usertype);
-    pageright.input('Menu_Id', sql.Int, menuid);
-    pageright.input('Menu_Type_Id', sql.Int, menutype);
-    const result = await pageright.execute('User_Rights_By_Page_Id');
-    if (result.recordset.length !== 0) {
-      res.status(200).json({
-        status: "success",
-        data: {
-          Read_Rights: result.recordset[0].Read_Rights,
-          Add_Rights: result.recordset[0].Add_Rights,
-          Edit_Rights: result.recordset[0].Edit_Rights,
-          Delete_Rights: result.recordset[0].Delete_Rights
-        }, message: ""
-      })
-    } else {
-      res.status(200).json({ data: { Read_Rights: 0, Add_Rights: 0, Edit_Rights: 0, Delete_Rights: 0 }, status: "Success", message: "No Page Rights" })
-    }
-  }
-  catch (err) {
-    console.error('Error executing SQL query:', err);
-    res.status(500).json({ message: 'Internal Server Error', status: 'Failure', data: [] });
-  }
-});
 
 app.post('/api/newmenu', authenticateToken, (req, res) => {
   const { menuType, menuName, menuLink, mainMenuId, subMenuId } = req.body;
