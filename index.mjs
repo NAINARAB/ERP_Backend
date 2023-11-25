e16de5e1b0ec5eb484cb011d6867973876d27c2f
NAINARAB <karthickab77@gmail.com>
2023-11-25 16:44:25 +0530

    null

--- index.mjs
+++ index.mjs
@@ -1,16 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-
-const app = express();
-app.use(cors());
-app.use(bodyParser.json({ limit: '50mb' }));
-
-
-app.get('/', (req, res) => {
-  res.json({data:[], status: 'Success', message:''})
-})
-
 import userRoutes from './routes/masters/user.mjs';
 import loginRoute from './routes/login-logout/login.mjs';
 import SalesForceAPI from './routes/sales/sfapi.mjs';
@@ -19,6 +9,15 @@ import SidebarRoute from './routes/sidebar.mjs';
 import PageRights from './routes/login-logout/pageRights.mjs';
 import CompanyRoute from './routes/company.mjs';
 import losRoute from './routes/report/los.mjs';
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json({ limit: '50mb' }));
+
+
+app.get('/', (req, res) => {
+  res.json({data:[], status: 'Success', message:'SMT-ERP API HOME'})
+})
 
 app.use(userRoutes)
 app.use(loginRoute)
