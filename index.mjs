e72d888c79027750651040701bd0ee899ceb8ba8
NAINARAB <karthickab77@gmail.com>
2024-01-11 16:54:16 +0530

    purchase

--- index.mjs
+++ index.mjs
@@ -14,7 +14,6 @@ import PageRights from './routes/login-logout/pageRights.mjs';
 import CompanyRoute from './routes/company.mjs';
 import losRoute from './routes/report/los.mjs';
 import DesignationRoute from './routes/masters/designation.mjs';
-import apiData from './config/apis.mjs';
 import EmpAttanance from './routes/attanance.mjs';
 import purchaseOrederReport from './routes/report/purchaseorder.mjs';
 import CustomerRoute from './routes/masters/customer.mjs';
@@ -28,104 +27,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json({ limit: '50mb' }));
 
-
-// app.get('/', (req, res) => {
-//   const bg = (method) => {
-//     if(method === "GET"){
-//       return '#00CDAC'
-//     } else if (method === "POST"){
-//       return '#DD2476'
-//     } else if (method === "PUT"){
-//       return '#A890FE'
-//     } else {
-//       return '#FF61D2'
-//     }
-//   }
-//   const htmlContent = `
-//   <html lang="en">
-//     <head>
-//       <title>SMTERP API</title>
-//       <style>
-//           table {
-//               border: 1px solid black;
-//           }
-  
-//           th {
-//               border: 1px solid black;
-//               padding: 5px 20px;
-//           }
-  
-//           tr {
-//               border: 1px solid black;
-//               padding: 2px;
-//           }
-  
-//           td {
-//               border: 1px solid black;
-//               padding: 10px;
-//               text-align: center;
-//           }
-  
-//           @font-face {
-//               font-family: 'prosans';
-//               font-style: normal;
-//               font-weight: 400;
-//               src: local('Open Sans'), local('OpenSans'), url(https://fonts.gstatic.com/s/productsans/v5/HYvgU2fE2nRJvZ5JFAumwegdm0LZdjqr5-oayXSOefg.woff2) format('woff2');
-//           }
-  
-//           * {
-//               font-family: prosans;
-//               box-sizing: border-box;
-//           }
-  
-//           body {
-//               margin: 2em;
-//               background: linear-gradient(45deg, #EE9CA7, #FFDDE1);
-//           }
-  
-          
-//       </style>
-//     </head>
-  
-//     <body>
-//       <h2 style="margin-top: 2em;color: white; text-shadow: 0px 4px 4px black">SMT APIs</h2>
-//       <table>
-//           <thead>
-//               <tr>
-//                   <th>SNo</th>
-//                   <th>Method</th>
-//                   <th>API</th>
-//                   <th>Authorization</th>
-//                   <th>header</th>
-//                   <th>query</th>
-//                   <th>param</th>
-//                   <th>body</th>
-//               </tr>
-//           </thead>
-//           <tbody>
-//               ${apiData.map((item, index) => `
-//               <tr>
-//                   <td>${index + 1}</td><!--sno-->
-//                   <td style="background-color: ${bg(item.method)}">${item.method}</td><!--method-->
-//                   <td style="text-align: left">${item.api}</td><!--api-->
-//                   <td>${item.authorization}</td><!--authorization-->
-//                   <td>${item.header}</td><!--header-->
-//                   <td>${item.query}</td><!--query-->
-//                   <td>${item.param}</td><!--param-->
-//                   <td>${item.body}</td><!--body-->
-//               </tr>`
-//               ).join('')}
-//           </tbody>
-//       </table>
-//     </body>
-  
-//   </html>
-//   `;
-//   res.setHeader('Content-Type', 'text/html');
-//   res.send(htmlContent);
-// });
-
-app.use(userRoutes, loginRoute, SalesForceAPI, BranchROute, SidebarRoute, PageRights, CompanyRoute, losRoute, DesignationRoute,EmpAttanance, purchaseOrederReport, CustomerRoute, CustomerReportRoute)
+app.use(userRoutes, loginRoute, SalesForceAPI, BranchROute, SidebarRoute, PageRights, CompanyRoute, losRoute, DesignationRoute, EmpAttanance, purchaseOrederReport, CustomerRoute, CustomerReportRoute)
 
 const pathList = [
   './routes/masters/user.mjs',
@@ -142,12 +44,20 @@ const pathList = [
 ];
 
 const options = {
-  swaggerDefinition: swaggerDefinition, 
+  swaggerDefinition: {
+    info: {
+      title: 'SMT API\'s',
+      version: '1.0.0',
+      description: 'Description of your API',
+    },
+    host: 'localhost:5000',
+    basePath: '/',
+  },
   apis: pathList,
 };
 
 const swaggerSpec = swaggerJSDoc(options);
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+app.use('/api', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 
 
--- routes/report/purchaseorder.mjs
+++ routes/report/purchaseorder.mjs
@@ -64,4 +64,30 @@ purchaseOrederReport.get('/api/PurchaseOrderReport', dbconnect, authenticateToke
     }
 }) 
 
+purchaseOrederReport.get('/api/PurchaseOrderItemBased', dbconnect, authenticateToken, async (req, res) => {
+    try {
+        const { Report_Type, Fromdate, Todate, Customer_Id, Item_Id, BillNo } = req.query;
+        const guid = req.config.Tally_Guid;
+
+        const DynamicDB = new sql.Request(req.db);
+        DynamicDB.input('Report_Type', Report_Type);
+        DynamicDB.input('Guid', guid);
+        DynamicDB.input('Fromdate', Fromdate)
+        DynamicDB.input('Todate', Todate);
+        DynamicDB.input('Customer_Id', Customer_Id);
+        DynamicDB.input('Stock_Group_Id', 0);
+        DynamicDB.input('Item_Id', Item_Id);
+        DynamicDB.input('BillNo', BillNo);
+
+        const result = await DynamicDB.execute('Purchase_Order_Report_Search');
+        if(result.recordset.length > 0){
+            res.json({data: result.recordset, status: 'Success', message: ''})
+        } else {
+            res.json({data: [], status: 'Success', message: 'No Data'})
+        }
+    } catch (e){
+        ServerError(e, '/api/PurchaseOrderReport', 'GET', res)
+    }
+})
+
 export default purchaseOrederReport;
